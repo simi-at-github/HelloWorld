377d333371e0f51d319a0bd895ee07d7df0fe3fa
Schierling <uidv5267@contiwan.com>
2020-04-01 12:22:39 +0200

    asdf

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,11 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        if(args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }        
     }
 
 }
